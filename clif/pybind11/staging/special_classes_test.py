6ffefafce22970e328f1a914fc8b71807bd15d89
wangxf <wangxf@google.com>
2020-12-28 12:01:55 -0800

    Add pybind11 bindings code for special classes.
    Those classes are taken out from t2.h.
    
    PiperOrigin-RevId: 349313834

---
+++ clif/pybind11/staging/special_classes_test.py
@@ -0,0 +1,53 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for clif.pybind11.staging.special_classes.
+
+This file is a copy of clif/testing/python/special_classes_test.
+"""
+
+import unittest
+from clif.pybind11.staging import special_classes
+
+
+class SpecialClassesTest(unittest.TestCase):
+
+  def testAbstract(self):
+    self.assertTrue(special_classes.Abstract.Future)
+    self.assertRaises(TypeError, special_classes.Abstract)
+    self.assertEqual(special_classes.Abstract.KIND, 'pure virtual',
+                     str(type(special_classes.Abstract.KIND)))
+
+  def testInconstructibleStaticMethod(self):
+    self.assertEqual(special_classes.InconstructibleF(), 0)
+
+  def testNoDefaultConstructor(self):
+    with self.assertRaises(TypeError):
+      # true error, no def ctor
+      _ = special_classes.NoDefaultConstructor().a
+    with self.assertRaises(TypeError):
+      # true error, non-def ctor not wrapped
+      _ = special_classes.NoDefaultConstructor(1).a
+
+  def testNoCopy(self):
+    no_copy = special_classes.NoCopy(1)
+    self.assertEqual(no_copy.a, 1)
+
+  def testNoMove(self):
+    no_move = special_classes.NoMove(1)
+    self.assertEqual(no_move.a, 1)
+
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ clif/testing/special_classes.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef THIRD_PARTY_CLIF_TESTING_SPECIAL_CLASSES_H_
+#define THIRD_PARTY_CLIF_TESTING_SPECIAL_CLASSES_H_
+
+namespace clif_testing {
+namespace special_classes {
+
+class Abstract {
+  Abstract() {}
+ public:
+  static constexpr char KIND[] = "pure virtual";
+  virtual ~Abstract() {}
+  virtual int Future() = 0;
+};
+
+class Inconstructible {
+  Inconstructible() {}
+ public:
+  static int F() { return 0; }
+};
+
+class NoDefaultConstructor {
+  int a_;
+ public:
+  explicit NoDefaultConstructor(int a) : a_(a) {}
+  int A() { return a_; }
+};
+
+// Class 'NoCopy' is not CLIF copyable because it does not contain a copy
+// constructor or a copy assignment operator.
+class NoCopy {
+  int a_;
+  NoCopy(const NoCopy&) = delete;
+  NoCopy& operator=(const NoCopy&) = delete;
+ public:
+  explicit NoCopy(int a = 0) : a_(a) {}
+  int A() { return a_; }
+};
+
+// Class 'NoMove' is not CLIF movable because it does not contain a move
+// constructor or a move assignment operator.
+class NoMove {
+  int a_;
+  NoMove(const NoMove&) = default;
+  NoMove& operator=(const NoMove&) = default;
+  NoMove(NoMove&&) = delete;
+  NoMove& operator=(NoMove&&) = delete;
+ public:
+  explicit NoMove(int a = 0) : a_(a) {}
+  int A() { return a_; }
+};
+
+}  // namespace special_classes
+}  // namespace clif_testing
+
+#endif  // THIRD_PARTY_CLIF_TESTING_SPECIAL_CLASSES_H_
