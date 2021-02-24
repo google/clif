d890da641b4f05cc78d03f86aa041d3f1ff4e6b0
wangxf <wangxf@google.com>
2021-02-24 15:08:51 -0800

    Add pybind11 bindings code for smart_ptrs.
    PiperOrigin-RevId: 359379589

---
+++ clif/pybind11/staging/smart_ptrs.cc
@@ -0,0 +1,103 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include "clif/testing/smart_ptrs.h"
+
+#include "third_party/pybind11/include/pybind11/smart_holder.h"
+
+namespace py = pybind11;
+
+class PyOperation : public ::smart_ptrs::Operation {
+ public:
+  using ::smart_ptrs::Operation::Operation;
+  int Run() override {
+    PYBIND11_OVERRIDE_PURE(
+        int,      /* Return type */
+        ::smart_ptrs::Operation,         /* Parent class */
+        Run     /* Name of function in C++ (must match Python name) */
+    );
+  }
+};
+
+PYBIND11_SMART_HOLDER_TYPE_CASTERS(::smart_ptrs::A)
+PYBIND11_SMART_HOLDER_TYPE_CASTERS(::smart_ptrs::B)
+PYBIND11_SMART_HOLDER_TYPE_CASTERS(::smart_ptrs::Operation)
+PYBIND11_SMART_HOLDER_TYPE_CASTERS(::smart_ptrs::C1)
+PYBIND11_SMART_HOLDER_TYPE_CASTERS(::smart_ptrs::D1)
+PYBIND11_SMART_HOLDER_TYPE_CASTERS(::smart_ptrs::WithPrivateDtor)
+PYBIND11_SMART_HOLDER_TYPE_CASTERS(::smart_ptrs::X)
+
+PYBIND11_MODULE(smart_ptrs, m) {
+  py::classh<::smart_ptrs::A>(m, "A")
+    .def(py::init<>())
+    .def_readwrite("a", &::smart_ptrs::A::a_);
+
+  py::classh<::smart_ptrs::B>(m, "B")
+    .def(py::init<>())
+    .def("Get",
+         (std::shared_ptr<::smart_ptrs::A> (::smart_ptrs::B::*)())
+         &::smart_ptrs::B::Get)
+    .def("GetNew",
+         (std::unique_ptr<::smart_ptrs::A> (::smart_ptrs::B::*)())
+         &::smart_ptrs::B::GetNew)
+    .def("Set",
+         (void (::smart_ptrs::B::*)(::smart_ptrs::A))
+         &::smart_ptrs::B::Set, py::arg("a"))
+    .def("SetSP",
+         (void (::smart_ptrs::B::*)(std::shared_ptr<::smart_ptrs::A>))
+         &::smart_ptrs::B::SetSP, py::arg("a"));
+
+  m.def("Func",
+        (::smart_ptrs::B (*)(std::unique_ptr<::smart_ptrs::A>))
+        &::smart_ptrs::Func, py::arg("a"));
+
+  py::classh<::smart_ptrs::Operation, PyOperation>(m, "Operation")
+    .def(py::init<>())
+    .def("Run",
+         (int (::smart_ptrs::Operation::*)())&::smart_ptrs::Operation::Run);
+
+  m.def("PerformUP",
+        (int (*)(std::unique_ptr<::smart_ptrs::Operation>))
+        &::smart_ptrs::PerformUP, py::arg("op"));
+  m.def("PerformSP",
+        (int (*)(std::shared_ptr<::smart_ptrs::Operation>))
+        &::smart_ptrs::PerformSP, py::arg("op"));
+
+  // TODO: Temporarily comment out the bindings code here because
+  // currently pybind11 does not support calling protected or private
+  // destructors.
+  //   py::classh<::smart_ptrs::C1>(m, "C1")
+  //     .def("Get",
+  //          (int (::smart_ptrs::C1::*)())&::smart_ptrs::C1::Get);
+
+  //   py::classh<::smart_ptrs::D1, ::smart_ptrs::C1>(m, "D1")
+  //     .def(py::init<int>(), py::arg("i"));
+
+  //   py::classh<::smart_ptrs::WithPrivateDtor>(m, "WithPrivateDtor")
+  //     .def_static("New",
+  //          (std::shared_ptr<::smart_ptrs::WithPrivateDtor> (*)())
+  //          &::smart_ptrs::WithPrivateDtor::New)
+  //     .def("Get",
+  //          (int (::smart_ptrs::WithPrivateDtor::*)())
+  //          &::smart_ptrs::WithPrivateDtor::Get);
+
+  py::classh<::smart_ptrs::X>(m, "X")
+    .def(py::init<>())
+    .def_readwrite("y", &::smart_ptrs::X::y);
+
+  m.def("F3",
+        (std::unique_ptr<::smart_ptrs::X> (*)(std::unique_ptr<::smart_ptrs::X>))
+        &::smart_ptrs::F3, py::arg("x"));
+}
---
+++ clif/pybind11/staging/smart_ptrs_test.py
@@ -0,0 +1,111 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for clif.pybind11.staging.smart_ptrs."""
+
+import unittest
+from clif.pybind11.staging import smart_ptrs
+
+
+class Add(smart_ptrs.Operation):
+
+  def __init__(self, a, b):
+    smart_ptrs.Operation.__init__(self)
+    self.a = a
+    self.b = b
+
+  def Run(self):
+    return self.a + self.b
+
+
+class SmartPtrsTest(unittest.TestCase):
+
+  def testSmartPtrs(self):
+    a = smart_ptrs.A()
+    a.a = 123
+    b = smart_ptrs.B()
+    b.Set(a)
+    self.assertEqual(b.Get().a, 123)
+    self.assertEqual(b.GetNew().a, 123)
+
+    # |a| is not shared with C++. So, the below call to Func should invalidate
+    # it.
+    b = smart_ptrs.Func(a)
+    self.assertEqual(b.Get().a, 123)
+    with self.assertRaises((ValueError, RuntimeError)):
+      _ = a.a
+    with self.assertRaises((ValueError, RuntimeError)):
+      smart_ptrs.Func(a)
+
+    a = smart_ptrs.A()
+    a.a = 54321
+    b.SetSP(a)
+    # |a| is shared between C++ and Python. So, cannot give it to Func.
+    with self.assertRaises((ValueError, RuntimeError)):
+      smart_ptrs.Func(a)
+
+    # Check |a| is intact.
+    self.assertEqual(a.a, 54321)
+
+    # Remove the reference in |b|
+    b.Set(a)
+    # We should be able to call Func again
+    b = smart_ptrs.Func(a)
+    self.assertEqual(b.Get().a, 54321)
+    with self.assertRaises((ValueError, RuntimeError)):
+      _ = a.a
+    with self.assertRaises((ValueError, RuntimeError)):
+      smart_ptrs.Func(a)
+
+    # TODO: Temporarily disable the test because pybind11 throws
+    # `RuntimeError: Tried to call pure virtual function` when calling
+    # smart_ptrs.PerformUP. However, smart_ptrs.PerformSP works. This might be
+    # because pybind11 does not support unique_ptr as function parameters
+    # previously.
+    # add = Add(120, 3)
+    # self.assertEqual(smart_ptrs.PerformUP(add), 123)
+
+    # # Previous call to Perform invalidated |add|
+    # with self.assertRaises((ValueError, RuntimeError)):
+    #   smart_ptrs.PerformUP(add)
+
+    add = Add(1230, 4)
+    self.assertEqual(smart_ptrs.PerformSP(add), 1234)
+    # Calls to PerformSP should not invalidate |add|.
+    self.assertEqual(smart_ptrs.PerformSP(add), 1234)
+
+    # TODO: Temporarily disable the test because currently pybind11
+    # does not support calling protected destructor.
+    # self.assertEqual(smart_ptrs.D1(123).Get(), 123)
+
+  # TODO: Disable the test. See skip reason.
+  @unittest.skip(
+      'Temporarily disable the test because currently pybind11 does not support'
+      'calling private destructor.')
+  def testPrivateDtor(self):
+    # Can deal with objects with private/protected destructor std::shared_ptr.
+    d = smart_ptrs.WithPrivateDtor.New()
+    self.assertEqual(d.Get(), 321)
+
+  def testF3(self):
+    x = smart_ptrs.X()
+    x.y = 123
+    x1 = smart_ptrs.F3(x)
+    self.assertEqual(x1.y, 123)
+    with self.assertRaises((ValueError, RuntimeError)):
+      _ = x.y
+
+
+if __name__ == '__main__':
+  unittest.main()
