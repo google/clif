9210aeaba689a1e42dce1153c5fabbbda4c2a8dd
kavii <kavii@google.com>
2021-02-24 10:24:21 -0800

    Extend_default_value_test passes.
    PiperOrigin-RevId: 359316361

--- clif/testing/python/extend_default_value_test.py
+++ clif/testing/python/extend_default_value_test.py
@@ -18,47 +18,60 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import unittest
+from absl.testing import absltest
+from absl.testing import parameterized
 
 from clif.testing.python import extend_default_value
+# TODO: Restore simple import after OSS setup includes pybind11.
+# pylint: disable=g-import-not-at-top
+try:
+  from clif.testing.python import extend_default_value_pybind11
+except ImportError:
+  extend_default_value_pybind11 = None
+# pylint: enable=g-import-not-at-top
 
 
-class ExtendDefaultValueTest(unittest.TestCase):
+@parameterized.named_parameters([
+    np for np in zip(('c_api', 'pybind11'), (extend_default_value,
+                                             extend_default_value_pybind11))
+    if np[1] is not None
+])
+class ExtendDefaultValueTest(absltest.TestCase):
 
-  def test_only_default_values(self):
+  def test_only_default_values(self, wrapper_lib):
     v1 = 10
     v2 = 100
     expected_value = v1 + v2
-    abc = extend_default_value.Abc(v1)
+    abc = wrapper_lib.Abc(v1)
     self.assertEqual(abc.get_value(), v1)
     abc.sum_and_set_values()
     self.assertEqual(abc.get_value(), expected_value)
 
-  def test_one_default_value(self):
+  def test_one_default_value(self, wrapper_lib):
     v1 = 123
     v2 = 100
     expected_value = v1 + v2
-    abc = extend_default_value.Abc(v1)
+    abc = wrapper_lib.Abc(v1)
     self.assertEqual(abc.get_value(), v1)
     abc.sum_and_set_values(v1=v1)
     self.assertEqual(abc.get_value(), expected_value)
 
-  def test_not_using_default_value(self):
+  def test_not_using_default_value(self, wrapper_lib):
     v1 = 123
     v2 = 456
     expected_value = v1 + v2
-    abc = extend_default_value.Abc(v1)
+    abc = wrapper_lib.Abc(v1)
     self.assertEqual(abc.get_value(), v1)
     abc.sum_and_set_values(v1=v1, v2=v2)
     self.assertEqual(abc.get_value(), expected_value)
 
-  def test_default_value_in_constructor(self):
+  def test_default_value_in_constructor(self, wrapper_lib):
     expected_value = 10
-    obj = extend_default_value.DefaultValueInConstructor()
+    obj = wrapper_lib.DefaultValueInConstructor()
     self.assertEqual(obj.value, expected_value)
-    obj = extend_default_value.DefaultValueInConstructor(123)
+    obj = wrapper_lib.DefaultValueInConstructor(123)
     self.assertEqual(obj.value, 123)
 
 
 if __name__ == '__main__':
-  unittest.main()
+  absltest.main()
