9cda5975a70f2124d0b8ab181031847414fc25c8
wangxf <wangxf@google.com>
2021-03-11 12:37:45 -0800

    Make cindex extractor support checking overloaded functions.
    Functions with parameters like reference to pointers, rvalue references, pointer to pointers, pointer to constants might not work properly.
    
    This CL introduces very low-level manipulations of C++ type names (stripping spaces, stars, ampersands) that are difficult to generalize and maintain. We are exploring alternative approaches that may allow us to avoid such manipulations. In the meantime this CL unblocks the work on the pybind11 code generator.
    
    PiperOrigin-RevId: 362354347

--- clif/pybind11/cindex/extractor.py
+++ clif/pybind11/cindex/extractor.py
@@ -1,4 +1,10 @@
-"""Implementation of a cindex based extractor."""
+"""Implementation of a cindex based extractor.
+
+The cindex based extractor contains very low-level manipulations of
+C++ type names (stripping spaces, stars, ampersands) that are
+difficult to generalize and maintain. We are exploring alternative
+approaches that may allow us to avoid such manipulations.
+"""
 
 from typing import List, Optional, Text
 
@@ -9,11 +15,187 @@ from clang.cindex import TypeKind
 from clif.protos import ast_pb2
 
 
+def _get_pointee_name(type_name: Text) -> Text:
+  """Remove the trailing ampersands and stars in type names."""
+  type_name = type_name.strip()
+  if type_name.endswith('*'):
+    type_name = type_name.split('*')[0].strip()
+  if type_name.endswith('&'):
+    type_name = type_name.split('&')[0].strip()
+  const_identifier = 'const '
+  if type_name.startswith(const_identifier):
+    type_name = const_identifier + type_name[len(const_identifier):].strip()
+  return type_name
+
+
+class Type(object):
+  """Wraps a clang or a CLIF type."""
+
+  def __init__(self, name: Text, is_pointer: bool, is_reference: bool,
+               is_const: bool, pointee):
+    self._name = name
+    self._is_pointer = is_pointer
+    self._is_reference = is_reference
+    self._is_const = is_const
+    self._pointee = pointee
+
+  @classmethod
+  def from_clif(cls, param_decl: ast_pb2.ParamDecl):
+    """Creates a type object from CLIF ast."""
+    name = param_decl.cpp_exact_type
+    pointee_name = _get_pointee_name(name)
+    # TODO: Make this handle reference to pointers,
+    # rvalue references, pointer to pointers, pointer to constants.
+    is_pointer = param_decl.cpp_exact_type.strip().endswith('*')
+    is_reference = param_decl.cpp_exact_type.strip().endswith('&')
+    is_const = param_decl.cpp_exact_type.strip().startswith('const ')
+    if is_pointer or is_reference:
+      pointee = Type(pointee_name, False, False, is_const, None)
+    else:
+      pointee = None
+    return cls(name, is_pointer, is_reference, is_const, pointee)
+
+  @classmethod
+  def from_cindex(cls, cindex_type):
+    """Creates a type object from Clang."""
+    kind = cindex_type.kind
+
+    is_pointer = kind == TypeKind.POINTER
+    is_reference = kind == TypeKind.LVALUEREFERENCE
+    if is_pointer or is_reference:
+      pointee = Type.from_cindex(cindex_type.get_pointee())
+      is_const = cindex_type.get_pointee().is_const_qualified()
+    else:
+      pointee = None
+      is_const = cindex_type.is_const_qualified()
+    name = cindex_type.spelling
+    # This aims to make the object names consistent with CLIF. CLIF always add
+    # global namespace prefix to objects. For example, using '::A' for object
+    # 'A'. We also want to make the Type object created from cindex follow the
+    # same rule.
+    if kind == TypeKind.ELABORATED or kind == TypeKind.RECORD:
+      if is_const:
+        name = 'const ::' + name.split(' ')[-1]
+      else:
+        name = '::' + name
+    return cls(name, is_pointer, is_reference, is_const, pointee)
+
+  @classmethod
+  def void(cls):
+    return cls('void', False, False, False, None)
+
+  def __eq__(self, other) -> bool:
+    if self._is_pointer != other._is_pointer:
+      return False
+    if self._is_reference != other._is_reference:
+      return False
+    if self._is_const != other._is_const:
+      return False
+    if self._is_pointer or self._is_reference:
+      return self._pointee == other._pointee
+    else:
+      return self._name == other._name
+
+  def __repr__(self):
+    pointee_name = self._pointee.name if self._pointee else 'None'
+    return (
+        f'Type name={self._name}, is_pointer={self._is_pointer}, '
+        f'is_reference={self._is_reference}, is_const={self._is_const}, '
+        f'pointee={pointee_name}')
+
+  @property
+  def name(self):
+    return self._name
+
+  @property
+  def pointee_name(self):
+    if self._pointee:
+      return self._pointee.name
+    else:
+      return ''
+
+
+class Function(object):
+  """Wraps a clang or a CLIF Function."""
+
+  def __init__(self, fq_name: Text, is_pure_virtual: bool,
+               arguments: List[Type], return_type: List[Type]):
+    self._fq_name = fq_name
+    self._is_pure_virtual = is_pure_virtual
+    self._arguments = arguments
+    self._return_type = return_type
+    self._is_overloaded = False
+
+  @classmethod
+  def from_clif(cls, func_decl: ast_pb2.Decl):
+    """Create a Function object from CLIF func_decl proto."""
+    fq_name = func_decl.func.name.cpp_name
+    is_pure_virtual = func_decl.func.is_pure_virtual
+    arguments = [Type.from_clif(x) for x in func_decl.func.params]
+    if not func_decl.func.returns:
+      return_type = [Type.void()]
+    else:
+      return_type = [Type.from_clif(x) for x in func_decl.func.returns]
+    return cls(fq_name, is_pure_virtual, arguments, return_type)
+
+  @classmethod
+  def from_cindex(cls, cursor: Cursor, namespace: Text):
+    """Create a Type object from cindex cursor."""
+    fq_name = '::'.join([namespace, cursor.spelling])
+    is_pure_virtual = cursor.is_pure_virtual_method()
+    arguments = [
+        Type.from_cindex(x) for x in cursor.type.argument_types()
+    ]
+    if cursor.type == TypeKind.VOID:
+      return_type = [Type.void()]
+    else:
+      return_type = [
+          Type.from_cindex(cursor.type.get_result())
+      ]
+    return cls(fq_name, is_pure_virtual, arguments, return_type)
+
+  def __eq__(self, other) -> bool:
+    if self._fq_name != other._fq_name:
+      return False
+    if len(self._arguments) != len(other._arguments):
+      return False
+    for x, fx in zip(self._arguments, other._arguments):
+      if x != fx:
+        return False
+    return True
+
+  def __repr__(self):
+    args = ', '.join([f'{a.name}{{{a.pointee_name}}}' for a in self._arguments])
+    returns = ', '.join(
+        [f'{r.name}{{{r.pointee_name}}}' for r in self._return_type])
+    return (
+        f'Function fq_name={self._fq_name}, '
+        f'is_pure_virtual={self._is_pure_virtual}, '
+        f'is_overloaded={self._is_overloaded}, '
+        f'arguments={args}, return_type={returns}')
+
+  @property
+  def fq_name(self):
+    return self._fq_name
+
+  @property
+  def is_pure_virtual(self):
+    return self._is_pure_virtual
+
+  @property
+  def is_overloaded(self):
+    return self._is_overloaded
+
+  @is_overloaded.setter
+  def is_overloaded(self, is_overloaded: bool) -> None:
+    self._is_overloaded = is_overloaded
+
+
 class Module(object):
   """Wraps a clang AST."""
 
   def __init__(self, tu: TranslationUnit):
-    self._func_decl_map = {}
+    self._func_decls_map = {}
     self._class_decl_map = {}
 
     self._traverse_namespace(tu.cursor, [])
@@ -32,8 +214,8 @@ class Module(object):
       raise ValueError(f'Errors in source file: {err_msg}')
     return cls(tu)
 
-  def query_func(self, fully_qualified_name: Text) -> Optional[ast_pb2.Decl]:
-    return self._func_decl_map.get(fully_qualified_name)
+  def query_func(self, fully_qualified_name: Text) -> List[Function]:
+    return self._func_decls_map.get(fully_qualified_name, [])
 
   def query_class(self, fully_qualified_name: Text) -> Optional[ast_pb2.Decl]:
     return self._class_decl_map.get(fully_qualified_name)
@@ -45,6 +227,7 @@ class Module(object):
       cursor: The cursor which is pointing to the head of the namespace.
       namespaces: The parent namespace in which the current namespace is in.
     """
+    # TODO: Make this handle function or class templates.
     for c in cursor.get_children():
       if c.kind in (CursorKind.CLASS_DECL, CursorKind.STRUCT_DECL):
         self._traverse_class(c, namespaces)
@@ -57,7 +240,7 @@ class Module(object):
         self._traverse_namespace(c, child_namespaces)
 
   def _traverse_function(self, cursor: Cursor,
-                         namespaces: List[Text]) -> ast_pb2.Decl:
+                         namespaces: List[Text]) -> None:
     """Traverses a function in clang AST and registers its declaration.
 
     This method modifies self._func_decl_map.
@@ -66,19 +249,16 @@ class Module(object):
       cursor: The cursor which is pointing to the head of a function in clang
         AST.
       namespaces: The namespace in which the function is in.
-
-    Returns:
-      The function declaration. This is needed so that the class decl which
-        defines this function can add it as a member.
     """
-    func_decl = ast_pb2.Decl()
-    func_decl.decltype = ast_pb2.Decl.Type.FUNC
-    func_decl.func.is_pure_virtual = cursor.is_pure_virtual_method()
     namespace = self._gen_namespace_str(namespaces)
-    fully_qualified_name = '::'.join([namespace, cursor.spelling])
-
-    self._func_decl_map[fully_qualified_name] = func_decl
-    return func_decl
+    func_obj = Function.from_cindex(cursor, namespace)
+    if func_obj.fq_name in self._func_decls_map:
+      func_obj.is_overloaded = True
+      if len(self._func_decls_map[func_obj.fq_name]) == 1:
+        self._func_decls_map[func_obj.fq_name][0].is_overloaded = True
+    else:
+      self._func_decls_map[func_obj.fq_name] = []
+    self._func_decls_map[func_obj.fq_name].append(func_obj)
 
   def _traverse_class(self, cursor: Cursor, namespaces: List[Text]) -> None:
     """Traverses a class in clang AST and registers its declaration.
@@ -98,8 +278,7 @@ class Module(object):
           and c.type.kind == TypeKind.FUNCTIONPROTO):
         child_namespaces = list(namespaces)
         child_namespaces.append(cursor.spelling)
-        func_decl = self._traverse_function(c, child_namespaces)
-        class_decl.class_.members.append(func_decl)
+        self._traverse_function(c, child_namespaces)
     self._class_decl_map[fully_qualified_name] = class_decl
 
   def _gen_namespace_str(self, namespaces: List[Text]) -> Text:
@@ -109,20 +288,29 @@ class Module(object):
       return ''
 
 
-def _complement_matcher_ast(ast: ast_pb2.AST, module: Module) -> ast_pb2.AST:
+def _fix_decl(decl: ast_pb2.Decl, module: Module) -> None:
+  candidates = module.query_func(decl.func.name.cpp_name)
+  function_obj = Function.from_clif(decl)
+  for candidate in candidates:
+    if function_obj == candidate:
+      decl.func.is_pure_virtual = candidate.is_pure_virtual
+      decl.func.is_overloaded = candidate.is_overloaded
+      return
+
+
+def _complement_matcher_ast(ast: ast_pb2.AST, module: Module) -> None:
   """Add the extra bits of information to an existing AST."""
   for decl in ast.decls:
     if decl.decltype == ast_pb2.Decl.Type.CLASS:
       for member_decl in decl.class_.members:
         if member_decl.decltype == ast_pb2.Decl.Type.FUNC:
-          func_decl = module.query_func(member_decl.func.name.cpp_name)
-          if func_decl:
-            member_decl.func.is_pure_virtual = func_decl.func.is_pure_virtual
-  return ast
+          _fix_decl(member_decl, module)
+    elif decl.decltype == ast_pb2.Decl.Type.FUNC:
+      _fix_decl(decl, module)
 
 
 def run(matcher_ast: ast_pb2.AST, cpp_source: Text,
         extra_flags: Optional[List[Text]] = None) -> ast_pb2.AST:
   module = Module.from_source(cpp_source, extra_flags)
-  matcher_ast = _complement_matcher_ast(matcher_ast, module)
+  _complement_matcher_ast(matcher_ast, module)
   return matcher_ast
--- clif/pybind11/cindex/extractor_test.py
+++ clif/pybind11/cindex/extractor_test.py
@@ -5,6 +5,147 @@ from clif.protos import ast_pb2
 from clif.pybind11.cindex import extractor
 
 
+def gen_func_proto(name, return_type=None, param_types=None):
+  func_decl = ast_pb2.Decl()
+  func_decl.decltype = ast_pb2.Decl.Type.FUNC
+  func_decl.func.name.cpp_name = name
+  if return_type:
+    func_decl.func.returns.append(ast_pb2.ParamDecl(
+        cpp_exact_type=return_type))
+  if param_types:
+    for param_type in param_types:
+      func_decl.func.params.append(ast_pb2.ParamDecl(
+          cpp_exact_type=param_type))
+  return func_decl
+
+
+class FunctionTest(absltest.TestCase):
+  """Test for extractor.Function object and extractor.Type object."""
+
+  def test_simple_function(self):
+    file_content = """
+      int func(int a, int b, float c) {
+        return 1;
+      }
+    """
+    module = extractor.Module.from_source(file_content)
+    clang_functions = module.query_func('::func')
+    clif_function_decl = gen_func_proto(
+        '::func', return_type='int', param_types=['int', 'int', 'float'])
+    clif_function = extractor.Function.from_clif(clif_function_decl)
+    self.assertEqual(clang_functions[0], clif_function)
+
+  def test_const_params_and_returns(self):
+    file_content = """
+      const int func(const int a, const float b) {
+        return 1;
+      }
+    """
+    module = extractor.Module.from_source(file_content)
+    clang_functions = module.query_func('::func')
+    clif_function_decl = gen_func_proto(
+        '::func', return_type='const int',
+        param_types=['const int', 'const float'])
+    clif_function = extractor.Function.from_clif(clif_function_decl)
+    self.assertEqual(clang_functions[0], clif_function)
+
+  def test_void_returns(self):
+    file_content = """
+      void func(int a) {
+        return;
+      }
+    """
+    module = extractor.Module.from_source(file_content)
+    clang_functions = module.query_func('::func')
+    clif_function_decl = gen_func_proto('::func', param_types=['int'])
+    clif_function = extractor.Function.from_clif(clif_function_decl)
+    self.assertEqual(clang_functions[0], clif_function)
+
+  def test_reference_params_and_returns(self):
+    # Leave empty spaces in type names to test whether we can handle them
+    # correctly.
+    file_content = """
+      const    int func( int  & a,  float  &    b) {
+        return 1;
+      }
+    """
+    module = extractor.Module.from_source(file_content)
+    clang_functions = module.query_func('::func')
+    clif_function_decl = gen_func_proto(
+        '::func', return_type=' const     int   ',
+        param_types=['int   & ', ' float     & '])
+    clif_function = extractor.Function.from_clif(clif_function_decl)
+    self.assertEqual(clang_functions[0], clif_function)
+
+  def test_pointer_params_and_returns(self):
+    # Leave empty spaces in type names to test whether we can handle them
+    # correctly.
+    file_content = """
+      const int * func( const  int  * a, float  *   b) {
+        return nullptr;
+      }
+    """
+    module = extractor.Module.from_source(file_content)
+    clang_functions = module.query_func('::func')
+    clif_function_decl = gen_func_proto(
+        '::func', return_type=' const     int   * ',
+        param_types=[' const  int   *', ' float     *'])
+    clif_function = extractor.Function.from_clif(clif_function_decl)
+    self.assertEqual(clang_functions[0], clif_function)
+
+  def test_object_params_and_returns(self):
+    # Leave empty spaces in type names to test whether we can handle them
+    # correctly.
+    file_content = """
+      class A {
+      };
+
+      class B {
+      };
+
+      const B * func( const  A   & a, const B *  b) {
+        return nullptr;
+      }
+    """
+    module = extractor.Module.from_source(file_content)
+    clang_functions = module.query_func('::func')
+    clif_function_decl = gen_func_proto(
+        '::func', return_type=' const ::B   * ',
+        param_types=[' const  ::A   &', ' const ::B *     '])
+    clif_function = extractor.Function.from_clif(clif_function_decl)
+    self.assertEqual(clang_functions[0], clif_function)
+
+  def test_multiple_namespaces_params_and_returns(self):
+    # Leave empty spaces in type names to test whether we can handle them
+    # correctly.
+    file_content = """
+      namespace a {
+        class A {
+        };
+      }  // namespace a
+
+      namespace b {
+      class B {
+      };
+      namespace c{
+      class C {
+      };
+      }  // namespace c
+      }  // namespace b
+
+      const b::c::C * func( const  a::A   & a, const b::B *  b) {
+        return nullptr;
+      }
+    """
+    module = extractor.Module.from_source(file_content)
+    clang_functions = module.query_func('::func')
+    clif_function_decl = gen_func_proto(
+        '::func', return_type=' const ::b::c::C   * ',
+        param_types=[' const  ::a::A   &', ' const ::b::B *     '])
+    clif_function = extractor.Function.from_clif(clif_function_decl)
+    self.assertEqual(clang_functions[0], clif_function)
+
+
 class ExtractorTest(absltest.TestCase):
 
   def _format_ast_proto(self, ast):
@@ -13,9 +154,54 @@ class ExtractorTest(absltest.TestCase):
       if decl.decltype == ast_pb2.Decl.Type.CLASS:
         for member_decl in decl.class_.members:
           if member_decl.decltype == ast_pb2.Decl.Type.FUNC:
-            lines.append(f'{member_decl.func.name.cpp_name}:is_pure_virtual:'
-                         f'{member_decl.func.is_pure_virtual}')
+            lines.append(
+                f'{member_decl.func.name.cpp_name}:'
+                f'{len(member_decl.func.params)}:'
+                f'is_pure_virtual:{member_decl.func.is_pure_virtual}')
     return '\n'.join(lines) + '\n'
+
+  def test_overloaded_function(self):
+    file_content = """
+      int func1(int a);
+      int func1(float a);
+      float func1(float a, float b);
+      void func2(int a);
+      void func2(int a, int b);
+      void func3(int a, int b);
+    """
+    module = extractor.Module.from_source(file_content)
+    candidates = module.query_func('::func1')
+    self.assertLen(candidates, 3)
+    for candidate in candidates:
+      self.assertTrue(candidate.is_overloaded)
+    candidates = module.query_func('::func2')
+    self.assertLen(candidates, 2)
+    for candidate in candidates:
+      self.assertTrue(candidate.is_overloaded)
+    candidates = module.query_func('::func3')
+    self.assertLen(candidates, 1)
+    for candidate in candidates:
+      self.assertFalse(candidate.is_overloaded)
+
+  def test_multiple_namespaces_overloaded_function(self):
+    file_content = """
+    namespace a {
+      int func1(int a);
+      float func1(int a, int b);
+      namespace b {
+        int func1(float a);
+      }
+    }  // namespace a
+    """
+    module = extractor.Module.from_source(file_content)
+    candidates = module.query_func('::a::func1')
+    self.assertLen(candidates, 2)
+    for candidate in candidates:
+      self.assertTrue(candidate.is_overloaded)
+    candidates = module.query_func('::a::b::func1')
+    self.assertLen(candidates, 1)
+    for candidate in candidates:
+      self.assertFalse(candidate.is_overloaded)
 
   def test_global_namespace_pure_virtual_function(self):
     file_content = """
@@ -32,10 +218,11 @@ class ExtractorTest(absltest.TestCase):
       };
     """
     module = extractor.Module.from_source(file_content)
-    self.assertTrue(module.query_func('::A::func1').func.is_pure_virtual)
-    self.assertFalse(module.query_func('::A::func2').func.is_pure_virtual)
-    self.assertFalse(module.query_func('::A::func3').func.is_pure_virtual)
-    self.assertFalse(module.query_func('::B::func4').func.is_pure_virtual)
+
+    self.assertTrue(module.query_func('::A::func1')[0].is_pure_virtual)
+    self.assertFalse(module.query_func('::A::func2')[0].is_pure_virtual)
+    self.assertFalse(module.query_func('::A::func3')[0].is_pure_virtual)
+    self.assertFalse(module.query_func('::B::func4')[0].is_pure_virtual)
 
   def test_nested_namespace_pure_virtual_function(self):
     file_content = """
@@ -58,10 +245,30 @@ class ExtractorTest(absltest.TestCase):
       };  // namespace c
     """
     module = extractor.Module.from_source(file_content)
-    self.assertTrue(module.query_func('::a::b::A::func1').func.is_pure_virtual)
-    self.assertFalse(module.query_func('::a::b::A::func2').func.is_pure_virtual)
-    self.assertFalse(module.query_func('::a::b::A::func3').func.is_pure_virtual)
-    self.assertFalse(module.query_func('::c::B::func4').func.is_pure_virtual)
+    self.assertTrue(module.query_func('::a::b::A::func1')[0].is_pure_virtual)
+    self.assertFalse(module.query_func('::a::b::A::func2')[0].is_pure_virtual)
+    self.assertFalse(module.query_func('::a::b::A::func3')[0].is_pure_virtual)
+    self.assertFalse(module.query_func('::c::B::func4')[0].is_pure_virtual)
+
+  def test_overloaded_function_and_pure_virtual(self):
+    file_content = """
+      class A {
+       public:
+        virtual int func1() = 0;
+        virtual float func1(int a) = 0;
+        virtual void func1(int a, int b);
+      };
+    """
+    module = extractor.Module.from_source(file_content)
+    candidates = module.query_func('::A::func1')
+    self.assertLen(candidates, 3)
+    for candidate in candidates:
+      if not candidate._arguments:
+        self.assertTrue(candidate.is_pure_virtual)
+      elif len(candidate._arguments) == 1:
+        self.assertTrue(candidate.is_pure_virtual)
+      else:
+        self.assertFalse(candidate.is_pure_virtual)
 
   def test_complement_ast(self):
     file_content = """
@@ -69,6 +276,7 @@ class ExtractorTest(absltest.TestCase):
         class A {
          public:
           virtual int func1() = 0;
+          virtual int func1(int a);
           virtual int func2();
         };
       };  // namespace a
@@ -77,19 +285,16 @@ class ExtractorTest(absltest.TestCase):
     ast = ast_pb2.AST()
     class_decl = ast_pb2.Decl()
     class_decl.decltype = ast_pb2.Decl.Type.CLASS
-    func_decl = ast_pb2.Decl()
-    func_decl.decltype = ast_pb2.Decl.Type.FUNC
-    func_decl.func.name.cpp_name = '::a::A::func1'
-    class_decl.class_.members.append(func_decl)
-    func_decl = ast_pb2.Decl()
-    func_decl.decltype = ast_pb2.Decl.Type.FUNC
-    func_decl.func.name.cpp_name = '::a::A::func2'
-    class_decl.class_.members.append(func_decl)
+    class_decl.class_.members.append(gen_func_proto('::a::A::func1', 'int'))
+    class_decl.class_.members.append(
+        gen_func_proto('::a::A::func1', 'int', ['int']))
+    class_decl.class_.members.append(gen_func_proto('::a::A::func2', 'int'))
     ast.decls.append(class_decl)
-    ast = extractor._complement_matcher_ast(ast, module)
+    extractor._complement_matcher_ast(ast, module)
     ast_str = self._format_ast_proto(ast)
-    self.assertIn('::a::A::func1:is_pure_virtual:True', ast_str)
-    self.assertIn('::a::A::func2:is_pure_virtual:False', ast_str)
+    self.assertIn('::a::A::func1:0:is_pure_virtual:True', ast_str)
+    self.assertIn('::a::A::func1:1:is_pure_virtual:False', ast_str)
+    self.assertIn('::a::A::func2:0:is_pure_virtual:False', ast_str)
 
 
 if __name__ == '__main__':
