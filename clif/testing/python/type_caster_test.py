257cc5597da5a90babdcccf7dcc729b742760383
wangxf <wangxf@google.com>
2021-06-29 10:45:54 -0700

    Fix type_caster test failure on Github.
    PiperOrigin-RevId: 382118721

--- clif/testing/python/type_caster_test.py
+++ clif/testing/python/type_caster_test.py
@@ -13,19 +13,32 @@
 # limitations under the License.
 
 from absl.testing import absltest
+from absl.testing import parameterized
 
-from clif.testing.python import type_caster_pybind11 as type_caster
+from clif.testing.python import type_caster
+# TODO: Restore simple import after OSS setup includes pybind11.
+# pylint: disable=g-import-not-at-top
+try:
+  from clif.testing.python import type_caster_pybind11
+except ImportError:
+  type_caster_pybind11 = None
+# pylint: enable=g-import-not-at-top
 
 
+@parameterized.named_parameters([
+    np for np in zip(('c_api', 'pybind11'), (type_caster,
+                                             type_caster_pybind11))
+    if np[1] is not None
+])
 class TypeCasterTest(absltest.TestCase):
 
-  def test_get_values(self):
-    self.assertEqual(type_caster.get_value_direct(10), 11)
-    self.assertEqual(type_caster.get_value_optional(12), 13)
-    self.assertEqual(type_caster.get_value_variant(14), 15)
-    self.assertEqual(type_caster.return_value(11), 12)
+  def test_get_values(self, wrapper_lib):
+    self.assertEqual(wrapper_lib.get_value_direct(10), 11)
+    self.assertEqual(wrapper_lib.get_value_optional(12), 13)
+    self.assertEqual(wrapper_lib.get_value_variant(14), 15)
+    self.assertEqual(wrapper_lib.return_value(11), 12)
     self.assertCountEqual(
-        type_caster.return_value_list([11, 12, 13]), [12, 13, 14])
+        wrapper_lib.return_value_list([11, 12, 13]), [12, 13, 14])
 
 
 if __name__ == '__main__':
