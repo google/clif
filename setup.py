fe0179ff51d04e731826d29181973b6096768288
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2023-06-09 21:37:05 -0700

    Go back to 20.04 (testing with 22.04 didn't work; needs more work).

--- setup.py
+++ setup.py
@@ -97,7 +97,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Code Generators',
     ],
