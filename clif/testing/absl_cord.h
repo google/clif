5f33586d3c17c93bb0cd4ad3eb6907ae8ac3e7be
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-03-31 13:41:58 -0700

    Do not use `std::string_view` in clif/testing/absl_cord.h
    To resolve this error:
    
    ```
    /clif/clif/testing/absl_cord.h:12:63: error: no matching conversion for functional-style cast from 'std::string_view' (aka 'basic_string_view<char>') to 'absl::Cord'
       12 | inline absl::Cord ReturnAbslCord(std::string_view v) { return absl::Cord(v); }
          |                                                               ^~~~~~~~~~~~
    ```

--- clif/testing/absl_cord.h
+++ clif/testing/absl_cord.h
@@ -2,14 +2,13 @@
 #define CLIF_TESTING_ABSL_CORD_H_
 
 #include <string>
-#include <string_view>
 
 #include "absl/strings/cord.h"
 
 namespace clif_testing_absl_cord {
 
 inline std::string PassAbslCord(const absl::Cord& c) { return std::string(c); }
-inline absl::Cord ReturnAbslCord(std::string_view v) { return absl::Cord(v); }
+inline absl::Cord ReturnAbslCord(const std::string& s) { return absl::Cord(s); }
 
 }  // namespace clif_testing_absl_cord
 
